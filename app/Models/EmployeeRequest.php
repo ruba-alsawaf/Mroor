d3ce25605cc6301a8d03ea909f4d28f33904790c
ruba-alsawaf <rubaalsawaf2003@gmail.com>
2025-01-04 02:08:33 +0300

    .

--- app/Models/EmployeeRequest.php
+++ app/Models/EmployeeRequest.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class EmployeeRequest extends Model
 {
 
-    protected $fillable = ['name', 'email'];
+    protected $fillable = ['name', 'email','password',];
 
 }
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', [EmployeeRequestController::class, 'index'])->name('dashboard');
+Route::get('/employee-requests', [EmployeeRequestController::class, 'index'])->name('employeeRequests.index');
 Route::get('accept-request/{id}', [EmployeeRequestController::class, 'acceptRequest'])->name('acceptRequest');
 Route::get('reject-request/{id}', [EmployeeRequestController::class, 'rejectRequest'])->name('rejectRequest');
 
