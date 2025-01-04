3be816c92c79d6866a5d0c65fcbc0899733cb96f
kinannotfound <kinannotfound2021@gmail.com>
2025-01-04 14:52:47 +0300

    created user and license models

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -42,5 +43,9 @@ class User extends Authenticatable
         return [
             'password' => 'hashed',
         ];
+    }
+    public function license() : BelongsTo
+    {
+        return $this->belongsTo(License::class);
     }
 }
