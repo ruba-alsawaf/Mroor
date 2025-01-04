46f91cd4ea1220c26a390a7c775faf3d1894da59
kinannotfound <kinannotfound2021@gmail.com>
2025-01-04 13:58:44 +0300

    added user and license tables

---
+++ database/migrations/2025_01_04_190108_create_licenses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('licenses', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->string('national_id')->unique();
+            $table->string('email')->unique();
+            $table->tinyInteger('points')->default(5);
+            $table->date('expiration_date');
+            $table->foreignId('employee_id')->references('id')->on('employees')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('licenses');
+    }
+};
