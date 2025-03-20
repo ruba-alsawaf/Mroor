9535f0bad2a9aa25ef3eccd9370fe8eaf41376a5
ruba-alsawaf <rubaalsawaf2003@gmail.com>
2025-03-21 02:48:40 +0300

    rr

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -13,6 +13,7 @@ use App\Mail\EmployeeApprovedMail;
 use App\Mail\ApprovalMail;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Exception;
 
 class EmployeeController extends Controller
 {
